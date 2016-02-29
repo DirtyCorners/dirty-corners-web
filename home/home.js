d9a22843ca80f8c340421975b5da618221ae6ffc
Khanh Hua <khanh.hua@pyco-group.com>
2016-02-29 21:19:36 +0700

    Initial commit with sample code from Auth0

---
+++ home/home.js
@@ -0,0 +1,34 @@
+angular.module( 'sample.home', [
+'auth0'
+])
+.controller( 'HomeCtrl', function HomeController( $scope, auth, $http, $location, store ) {
+
+  $scope.auth = auth;
+
+  $scope.callApi = function() {
+    // Just call the API as you'd do using $http
+    $http({
+      url: 'http://52.91.167.141:5000/api/incidents',
+      method: 'GET'
+    }).then(function(response) {
+      var data = response.data;
+
+      console.log("We got the secured data successfully", data);
+    }, function(response) {
+      if (response.status == 0) {
+        alert("Please download the API seed so that you can call it.");
+      }
+      else {
+        alert(response.data);
+      }
+    });
+  }
+
+  $scope.logout = function() {
+    auth.signout();
+    store.remove('profile');
+    store.remove('token');
+    $location.path('/login');
+  }
+
+});
